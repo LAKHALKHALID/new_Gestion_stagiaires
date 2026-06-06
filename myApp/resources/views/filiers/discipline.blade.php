11ea83853acebf545ef35437b4f7ca79c293bd0c
KHALID LAKHAL <khalidgit512@gmail.com>
2026-06-06 11:50:13 +0100

    just remove input

--- myApp/resources/views/filiers/discipline.blade.php
+++ myApp/resources/views/filiers/discipline.blade.php
@@ -8,12 +8,8 @@
     <div class="container">
         <form class="row g-3 align-items-center" action="{{route('discipline.afficher')}}" method="get">
             
-            <div class="col-sm-4">
-                <label class="" for="textInput">write the groupe</label>
-                <input type="text" class="form-control" id="textInput" placeholder="Enter text here...">
-            </div>
-
-            <div class="col-sm-4">
+            
+            <div class="col-sm-8">
                 <label class="" for="selectInput">Choose the filier</label>
                 <select name="filier" class="form-select" id="selectInput">
                     <option  value="all">All</option>
